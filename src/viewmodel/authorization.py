f542e14408541123c414f9c232d0456a1a1b6b37
Tishka17 <tishka17@mail.ru>
2017-12-18 18:36:02 +0300

    auth in viewmodel

---
+++ src/viewmodel/authorization.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import model.user
+
+import flask_jwt
+import werkzeug.security
+
+jwt = flask_jwt.JWT()
+
+
+def handle_auth(user):
+    if user \
+            and user.confirmed \
+            and not user.blocked:
+        access_token = jwt.jwt_encode_callback(user)
+        return jwt.auth_response_callback(access_token, identity)
+    else:
+        raise flask_jwt.JWTError('Bad Request', 'Invalid credentials')
+
+
+@jwt.identity_handler
+def identity(payload):
+    return payload["user_id"]
+
+
+@jwt.jwt_payload_handler
+def make_payload(idnt: model.user.User):
+    res = flask_jwt._default_jwt_payload_handler(idnt)
+    res.update({"user_id": idnt.id})
+    return res
+
+
+def auth_by_login(login, password):
+    user = model.user.User.query.filter_by(login=login).one_or_none()
+    if user \
+            and user.authorization \
+            and werkzeug.security.check_password_hash(user.authorization.password_hash, password):
+        return handle_auth(user)
+    else:
+        raise flask_jwt.JWTError('Bad Request', 'Invalid credentials')
