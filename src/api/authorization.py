dcc5f0e52eb149fea0446e236a723af01c5bb20c
Tishka17 <tishka17@mail.ru>
2018-01-16 19:40:59 +0300

    refresh token in api

--- src/api/authorization.py
+++ src/api/authorization.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import flask
+import flask_jwt_extended
 
 import use_cases.authorization
 from .blueprint import blueprint
@@ -10,19 +11,38 @@ from .blueprint import blueprint
 def auth_login():
     login = flask.request.form["login"]
     password = flask.request.form["password"]
-    access_token = use_cases.authorization.auth_by_login(login, password)
-    return flask.jsonify(access_token=access_token), 200
+    access_token, refresh_token = use_cases.authorization.auth_by_login(login, password)
+    return flask.jsonify(
+        access_token=access_token,
+        refresh_token=refresh_token
+    )
 
 
 @blueprint.route("/auth/vk", methods=["POST"])
 def auth_vk():
     code = flask.request.form["code"]
-    access_token = use_cases.authorization.auth_by_vk(code)
-    return flask.jsonify(access_token=access_token), 200
+    access_token, refresh_token = use_cases.authorization.auth_by_vk(code)
+    return flask.jsonify(
+        access_token=access_token,
+        refresh_token=refresh_token
+    )
 
 
 @blueprint.route("/auth/google", methods=["POST"])
 def auth_google():
     code = flask.request.form["code"]
-    access_token = use_cases.authorization.auth_by_google(code)
-    return flask.jsonify(access_token=access_token), 200
+    access_token, refresh_token = use_cases.authorization.auth_by_google(code)
+    return flask.jsonify(
+        access_token=access_token,
+        refresh_token=refresh_token
+    )
+
+
+@blueprint.route("/auth/refresh", methods=["POST"])
+@flask_jwt_extended.jwt_refresh_token_required
+def auth_refresh():
+    access_token, refresh_token = use_cases.authorization.refresh(flask_jwt_extended.get_jwt_identity())
+    return flask.jsonify(
+        access_token=access_token,
+        refresh_token=refresh_token
+    )
