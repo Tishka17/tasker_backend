e6c28ccd312a1e9828574c6e8ee0d19602a7bcfc
Tishka17 <tishka17@mail.ru>
2018-01-28 18:33:39 +0300

    facebook api

--- src/app.py
+++ src/app.py
@@ -20,12 +20,13 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 app.config['SECRET_KEY'] = os.environ['FLASK_SECRET_KEY']
 app.config['JWT_TOKEN_LOCATION'] = ['cookies', 'headers']
 app.config['JWT_COOKIE_CSRF_PROTECT'] = False
-app.config['VK_SECRET_KEY'] = os.environ['VK_SECRET_KEY']
 app.config['VK_CLIENT_ID'] = os.environ['VK_CLIENT_ID']
-app.config['GOOGLE_SECRET_KEY'] = os.environ['GOOGLE_SECRET_KEY']
+app.config['VK_SECRET_KEY'] = os.environ['VK_SECRET_KEY']
 app.config['GOOGLE_CLIENT_ID'] = os.environ['GOOGLE_CLIENT_ID']
+app.config['GOOGLE_SECRET_KEY'] = os.environ['GOOGLE_SECRET_KEY']
+app.config['FACEBOOK_CLIENT_ID'] = os.environ['FACEBOOK_CLIENT_ID']
+app.config['FACEBOOK_SECRET_KEY'] = os.environ['FACEBOOK_SECRET_KEY']
 app.config["BASE_URL"] = os.environ['BASE_URL']
-
 
 app.json_encoder = converters.json_encoder.CustomJSONEncoder
 app.jinja_env.globals['url_for_other_page'] = pages.template_filters.url_for_other_page
